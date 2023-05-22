fb4e63b8f36d0adbd470c6b36f123f4887a6af03
Hiago <hiagoisoppotrajano@gmail.com>
2023-05-22 00:39:57 -0300

    up

--- gerador-de-senhas/src/main.js
+++ gerador-de-senhas/src/main.js
@@ -1,8 +1,14 @@
-import { nanoid } from "nanoid";
+import './style.css';
+import { nanoid } from 'nanoid';
+import clipboardCopy from 'clipboard-copy';
 
 const generateBtn = document.querySelector('button');
 const displayPass = document.querySelector('h2');
 
 generateBtn.addEventListener('click', () => {
   displayPass.innerHTML = nanoid();
+});
+
+displayPass.addEventListener('click', (event) => {
+  clipboardCopy(event.target.innerHTML);
 });
