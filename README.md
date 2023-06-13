86c5ec8f5671198e8f6887eed6d57ed4f70cca4b
Hiago <hiagoisoppotrajano@gmail.com>
2023-06-13 00:34:43 -0300

    up

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Módulo: Front-end  
-### 3. Aprofundando no React  |  Dia 01: React Router
+### 3. Aprofundando no React  |  Dia 02: useEffect
 #### [Voltar ao sumário!](https://github.com/hiagoisoppo/trybe_exercicios/tree/main)
---
+++ useEffect/src/App.tsx
@@ -0,0 +1,49 @@
+import './App.css'
+import { useState, useEffect } from 'react';
+import { fetchCoordinates } from './services';
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+      setLoading(false);
+    }
+
+    fetchData()
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+    <h1>International Space Station Location Tracker</h1>
+    {coordinates && (
+      <>
+        <h2>{`Latitude: ${coordinates.latitude}`}</h2>
+        <h2>{`Longitude: ${coordinates.longitude}`}</h2>
+      </>
+    )}
+  </>
+  );
+}
+
+export default App;
